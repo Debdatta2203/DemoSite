7899eea7aa0c46441cf145b27b1b6be71e2b3d39
Debdatta2203 <debdatta@appinessworld.com>
2022-01-07 15:39:30 +0530

    Feat: new combination for screenshot included

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-// import { useEffect } from 'react';
-import { useEffect } from 'react';
+import React, { useState } from 'react';
 import Media from './components/media';
 import './style.css'
 
 const App = () => {
+  const [hideContent, setHideContent] = useState(false);
   //script to prevent screenshot
-  document.addEventListener("keyup", function (e) {
+  document.addEventListener("keydown", function (e) {
     var keyCode = e.key;
-    console.log('key pressed ', keyCode);
-    if (keyCode === 'Control' || keyCode === 'Meta' || keyCode === 'Alt' || keyCode === 'PrintScreen') {
-        alert('Screenshots not allowed');
+    console.log('key pressed ', e);
+    if (keyCode === 'Control' || 
+        keyCode === 'Meta' || 
+        keyCode === 'Alt' || 
+        keyCode === 'PrintScreen' || 
+        keyCode === 'Shift' ||
+        keyCode === '4' ||
+        keyCode === 'S' ||
+        keyCode === 's' ||
+        keyCode === 'P' ||
+        keyCode === 'p') {
+        setHideContent(true);
+        setTimeout(function(){
+          alert('Screenshots not allowed');
+          setHideContent(false);
+         }, 4000);
     }
   });
 
   return (
-      <div className="main-container">
+      <div className="main-container" style={{display: hideContent ? 'none' : ''}}>
           <Media />
       </div>
   );
